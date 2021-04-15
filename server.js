6a424cd1f1dacf9c84ece1fe80c2ef7b6c9be4a2
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-15 18:01:07 -0400

    added apiroutes and css styling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require ('fs');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
