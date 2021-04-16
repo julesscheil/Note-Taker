55090350d8cfb32a2cf05c4c80f3523e1ffb16ab
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-16 15:29:02 -0400

    fixed api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,11 +4,15 @@ const fs = require("fs");
 
 // export API Route
 module.exports = (app) => {
+
+  // get data in notes
   app.get("/api/notes", (req, res) => {
     let notesData = fs.readFileSync("db/db.json", "utf8");
     notesData = JSON.parse(notesData);
     res.json(notesData);
   });
+  //post data to notes
+
   app.post("/api/notes", (req, res) => {
     // read the notesData array from the db.json file
     let notesData = fs.readFileSync("db/db.json", "utf8");
@@ -22,7 +26,6 @@ module.exports = (app) => {
     // write the notesData array to the db.json file.
     fs.writeFileSync("db/db.json", JSON.stringify(notesData), "utf8");
     res.json(true);
-
   });
   
 
